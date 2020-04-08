0f9f250e7aa1c6ff4cb272d168ec8472688c86b8
w_dimuthu <w.dimuthu@directfn.com>
2020-04-08 22:36:39 +0530

    web site changes

---
+++ charts/diagnose/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import View 
+   
+from rest_framework.views import APIView 
+from rest_framework.response import Response 
+   
+class HomeView(View): 
+    def get(self, request, *args, **kwargs): 
+    	return render(request, 'diagnose/index.html') 
+
+class DiagnoseData(APIView): 
+    authentication_classes = [] 
+    permission_classes = [] 
+   
+    def get(self, request, format = None): 
+        data = {};
+        return Response(data) 
